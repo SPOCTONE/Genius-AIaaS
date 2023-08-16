6818f7224c979c8802e9abed2ddb7a88dfefc066
Subham Patel <47655017+SPOCTONE@users.noreply.github.com>
2023-08-16 17:10:48 +0530

    Made the API and UI of the Conversation AI

---
+++ components/loader.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export const Loader = () => {
+    return (
+        <div className="h-full flex flex-col gap-y-4 items-center justofy-center">
+            <div className="w-10 h-10 relative animate-spin">
+                <Image 
+                    alt="Loader"
+                    fill
+                    src="/logo.png"
+                />
+            </div>
+            <p className="text-sm text-muted-foreground">
+                Genius is thinking...
+            </p>
+        </div>
+    );
+};
